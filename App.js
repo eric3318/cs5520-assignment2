13e4913e3b77fd2b96a234449b073a2ff05a7d21
Han Nie <nie.han@northeastern.edu>
2024-10-11 15:18:25 -0700

    added stack navigation and wrapped context provider around

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import Activities from './screens/Activities';
+import { DataProvider } from './context/dataContext';
+import NewActivity from './screens/NewActivity';
+import Diet from './screens/Diet';
+
+const defaultScreenOptions = {
+  headerStyle: { backgroundColor: 'purple' },
+  headerTintColor: 'white',
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    /*    <View style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
+    <DataProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={defaultScreenOptions}>
+          <Stack.Screen
+            name="Activity"
+            component={Activities}
+            options={{
+              title: 'Activities',
+            }}
+          />
+          <Stack.Screen
+            name="New Activity"
+            component={NewActivity}
+            options={{
+              title: 'Add an Activity',
+            }}
+          />
+          <Stack.Screen
+            name="New Diet"
+            component={Diet}
+            options={{
+              title: 'Add a Diet Entry',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DataProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
