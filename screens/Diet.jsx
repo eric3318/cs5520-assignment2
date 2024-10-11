e96e166e1447f73dba4e279998b1b03a8fd5aad3
Han Nie <nie.han@northeastern.edu>
2024-10-11 15:42:42 -0700

    added input fields and datetime picker in NewDiet screen

--- screens/Diet.jsx
+++ screens/Diet.jsx
@@ -1,8 +1,26 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import ItemList from '../components/ItemList';
+import { useEffect } from 'react';
+import PressableButton from '../components/PressableButton';
 
-export default function Diet() {
-  return <View>
-    <ItemList items={} />
-  </View>;
+export default function Diet({ navigation, route }) {
+  const toAddScreen = () => {
+    navigation.push('New Diet');
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <PressableButton pressedFunction={toAddScreen}>
+          <Text>Add</Text>
+        </PressableButton>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View>
+      <ItemList />
+    </View>
+  );
 }
--- screens/NewDiet.jsx
+++ screens/NewDiet.jsx
@@ -1,5 +1,95 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { useData } from '../hook/useData';
+import DropDownPicker from 'react-native-dropdown-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import PressableButton from '../components/PressableButton';
 
 export default function NewDiet() {
-  return <View> </View>;
+  const [description, setDescription] = useState('');
+  const [calories, setCalories] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [data, setData] = useData();
+
+  const onChange = (event, selectedDate) => {
+    setDate(selectedDate);
+  };
+
+  const saveHandler = () => {
+    validateInput();
+    let newDiet = {
+      description,
+      calories,
+      date: date.toDateString(),
+    };
+    setData((prev) => {
+      return {
+        ...prev,
+        diet: [...prev.diet, newDiet],
+      };
+    });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const validateInput = () => {};
+
+  return (
+    <View style={styles.container}>
+      <Text>Description</Text>
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        style={styles.textArea}
+        multiline={true}
+      />
+      <Text>Calories</Text>
+      <TextInput
+        value={calories}
+        onChangeText={setCalories}
+        style={styles.textInput}
+      />
+      <Text>Date</Text>
+      <TextInput
+        editable={false}
+        style={styles.textInput}
+        value={date.toDateString()}
+      />
+      <DateTimePicker value={date} display="inline" onChange={onChange} />
+      <View style={styles.buttonContainer}>
+        <PressableButton>
+          <Text>Cancel</Text>
+        </PressableButton>
+        <PressableButton pressedFunction={saveHandler}>
+          <Text>Ok</Text>
+        </PressableButton>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    rowGap: 10,
+  },
+  textArea: {
+    borderWidth: 1,
+    borderRadius: 6,
+    height: 80,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  button: {},
+  textInput: {
+    borderWidth: 1,
+    paddingVertical: 14,
+    borderRadius: 6,
+  },
+});
