abc7a47c622f375058fe5a651f91279bea6258d7
Han Nie <nie.han@northeastern.edu>
2024-10-13 11:11:35 -0700

    added helper and updated themeContext

--- context/themeContext.js
+++ context/themeContext.js
@@ -1,12 +1,21 @@
 import { createContext, useState } from 'react';
+import { theme } from '../helper';
 
 export const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [currentTheme, setCurrentTheme] = useState(theme.light);
+
+  const toggleTheme = () => {
+    if (currentTheme === theme.light) {
+      setCurrentTheme(theme.dark);
+      return;
+    }
+    setCurrentTheme(theme.light);
+  };
 
   return (
-    <ThemeContext.Provider value={[theme, setTheme]}>
+    <ThemeContext.Provider value={[currentTheme, toggleTheme]}>
       {children}
     </ThemeContext.Provider>
   );
---
+++ helper.js
@@ -0,0 +1,10 @@
+export const theme = {
+  light: {
+    backgroundColor: '#caf0f8',
+    color: '#0077b6',
+  },
+  dark: {
+    backgroundColor: '#11001c',
+    color: '#3a015c',
+  },
+};
